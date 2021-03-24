cc7450d760b0fcb312fbb667d509384319039554
mathiax63 <mathyoyo@hotmail.es>
2021-03-24 14:29:08 -0300

    flebox

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const port = 3000
 const path = require('path');
-console.log('alberto probando')
+console.log('anda')
 app.use(express.static('public'));
 
 
@@ -11,5 +11,5 @@ app.get('/', (req, res) => {
 });
 
 app.listen(port, () => {
-    console.log('Example app listening on port '+ port);
+    console.log('esta andando en el puerto '+ port);
 });
